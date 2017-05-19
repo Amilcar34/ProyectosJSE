c7078a70cb8be72c8bfbeaff8b5af798ae802d3e
usuario <usuario@usuario-PC>
2017-05-19 20:44:40 -0300

    ¿Aca que va?

---
+++ Proyecto de prueba/src/Suma.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class Suma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a;
+		int b = 0;
+		String r;
+		
+		r = JOptionPane.showInputDialog("Introduce numero");
+		
+		
+		
+		System.out.print(r);
+
+	}
+
+}
